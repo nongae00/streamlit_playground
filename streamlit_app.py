42c1e09b39dd98f35e8b02f31e0634340e39f5bc
nongae00 <nongae00@hotmail.com>
2023-04-26 13:53:56 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,5 @@
 import streamlit as st
-import pandas as pd
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
 
-st.write(df)
+st.title('this is title')
+st.header('this is header')
+st.subheader('this is subheader')
