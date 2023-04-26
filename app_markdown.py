c950f6cf775d06d72d151f6192d1658e8a9c1c0a
nongae00 <nongae00@hotmail.com>
2023-04-26 14:15:03 +0800

    Create app_markdown.py

---
+++ app_markdown.py
@@ -0,0 +1,16 @@
+import streamlit as 
+
+## Markdown syntax
+st.markdown("# This is a Markdown title")
+st.markdown("## This is a Markdown header")
+st.markdown("### This is a Markdown subheader")
+st.markdown("- item 1\n"
+            "   - item 1.1\n"
+            "   - item 1.2\n"
+            "- item 2\n"
+            "- item 3")
+st.markdown("1. item 1\n"
+            "   1. item 1.1\n"
+            "   2. item 1.2\n"
+            "2. item 2\n"
+            "3. item 3")
